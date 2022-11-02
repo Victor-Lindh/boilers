b2eeb4323afc51890c66f7eb37a5bbfc1a6ace25
Victor Lindh <victorlindh@hotmail.com>
2022-11-03 00:14:44 +0100

    Make switching work better

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,19 +13,33 @@ const App = () => {
     dataLength: 'all',
   })
 
-  const currentCategoryData = categories[searchQuery.category]
-  const availableSubcategoryData = currentCategoryData.filter(item => item.subCategory === searchQuery.subCategory)
-  const groupData = availableSubcategoryData.find(x => x.data.value === searchQuery.group)
-  const data = groupData?.data.teams
+  const prefix = {
+    animals: 'animals',
+    sports: 'teams'
+  }
 
+  console.log('prefix', prefix[searchQuery.category])
+  console.log(searchQuery)
+  // Main category
+  const currentCategoryData = categories[searchQuery.category]
+  console.log('currentCategoryData', currentCategoryData)
+  // Sub category
+  const availableSubcategories = currentCategoryData.map(category => category.data.subCategory)
+  console.log('availableSubcategoryData', availableSubcategories)
+  // Group category
+  const groupData = currentCategoryData.filter(x => x.data.value === searchQuery.group)
+  console.log('groupdata', groupData)
+
+  const data = groupData[0]?.data?.[prefix[searchQuery.category]]
+  console.log('data', data);
   // return <div></div>
   return (
     <div className={styles.container}>
       <section className={styles.selectGroup}>
         <Select
           options={[
-            { value: 'array', label: 'Arrays' },
-            { value: 'object', label: 'Objects' },
+            { value: 'array', label: 'Plain array' },
+            { value: 'object', label: 'Array of objects' },
           ]}
           label='Type of data'
           onChange={e => setQuery({
@@ -34,21 +48,27 @@ const App = () => {
           })}
           />
             <Select
-              options={[
-                { value: 'sports', label: 'Sports' },
-                { value: 'animals', label: 'Animals' },
-              ]}
+              options={Object.keys(categories).map(category => {
+                return {
+                  value: category,
+                  label: `${category.charAt(0).toUpperCase()}${category.slice(1)}`
+                }
+              })}
               label='Category'
               onChange={e => setQuery({
                 ...searchQuery,
-                category: e.target.value
+                category: e.target.value,
+                subCategory: categories[e.target.value][0].subCategory,
+                group: categories[e.target.value][0].data.value
               })}
             />
         <Select
-          options={[
-            { value: 'football', label: 'Football' },
-            { value: 'basketball', label: 'Basketball' },
-          ]}
+          options={availableSubcategories.map(item => {
+            return {
+              value: item,
+              label: `${item.charAt(0).toUpperCase()}${item.slice(1)}`
+            }
+          })}
           label='Subcategory type'
           onChange={e => setQuery({
             ...searchQuery,
@@ -57,7 +77,7 @@ const App = () => {
           })}
           />
         <Select
-          options={availableSubcategoryData.map(item => item.data)}
+          options={groupData.map(item => item.data)}
           label='Group'
           onChange={e => setQuery({
             ...searchQuery,
@@ -70,7 +90,7 @@ const App = () => {
             { value: '5', label: 'First 5 items' },
             { value: '10', label: 'First 10 items' },
             { value: '15', label: 'First 15 items' },
-            { value: '20', label: 'First 20 items' },
+            { value: '20', label: 'First 20 items' }
           ]}
           label='Part of data'
           onChange={e => setQuery({
